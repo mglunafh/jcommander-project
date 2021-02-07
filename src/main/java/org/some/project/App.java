de8a7f8228998f05c3a611a5f90243b415fd738a
ufispb <ufispb@gmail.com>
2021-02-07 20:15:28 +0300

    added dependencies for jcommander

--- src/main/java/org/some/project/App.java
+++ src/main/java/org/some/project/App.java
@@ -1,13 +1,29 @@
 package org.some.project;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import lombok.Getter;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    HelloWorldArgs jArgs = new HelloWorldArgs();
+    JCommander helloCmd = JCommander.newBuilder()
+        .addObject(jArgs)
+        .build();
+    helloCmd.parse(args);
+    System.out.println("Hello " + jArgs.getName());
+  }
+
+  @Getter
+  public static class HelloWorldArgs {
+    @Parameter(
+        names = "--name",
+        description = "User name",
+        required = true
+    )
+    private String name;
+  }
 }
