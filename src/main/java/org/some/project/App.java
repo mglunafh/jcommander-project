1d697131982339d3cd041000c4cb8a5bd04d85b5
ufispb <ufispb@gmail.com>
2021-02-08 02:30:05 +0300

    fixed single executable jar

--- src/main/java/org/some/project/App.java
+++ src/main/java/org/some/project/App.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-
     HelloWorldArgs jArgs = new HelloWorldArgs();
     JCommander helloCmd = JCommander.newBuilder()
         .addObject(jArgs)
