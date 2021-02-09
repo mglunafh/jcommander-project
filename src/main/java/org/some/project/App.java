bd1a86e739fa25aefef3740e105c3ef57653fe41
ufispb <ufispb@gmail.com>
2021-02-10 01:17:42 +0300

    Figured out a problem with forceNonOverwritable paramaters in jCommander

--- src/main/java/org/some/project/App.java
+++ src/main/java/org/some/project/App.java
@@ -2,26 +2,50 @@ package org.some.project;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
-import lombok.Getter;
+import com.beust.jcommander.ParameterException;
 
 public class App {
 
   public static void main(String[] args) {
-    HelloWorldArgs jArgs = new HelloWorldArgs();
-    JCommander helloCmd = JCommander.newBuilder()
-        .addObject(jArgs)
+    String[] customArgs = { "-n", "Fedor", "-v", "15" };
+
+    RegularArgs regular = new RegularArgs();
+    JCommander regularResult = JCommander.newBuilder()
+        .addObject(regular)
+//        .allowParameterOverwriting(true)
         .build();
-    helloCmd.parse(args);
-    System.out.println("Hello " + jArgs.getName());
+    regularResult.parse(customArgs);
+    System.out.printf("Regular args: Hello  '%s'! Value: '%d'\n", regular.name, regular.value);
+
+    BrokenArgs broken = new BrokenArgs();
+    JCommander brokenResult = JCommander.newBuilder()
+        .addObject(broken)
+//        .allowParameterOverwriting(true)
+        .build();
+
+    try {
+      brokenResult.parse(customArgs);
+      System.out.printf(" Broken args: Hello  '%s'! Value: '%d'\n", broken.name, broken.value);
+    }
+    catch (ParameterException ex) {
+      System.out.println("Smth went wrong");
+      ex.printStackTrace();
+    }
   }
 
-  @Getter
-  public static class HelloWorldArgs {
-    @Parameter(
-        names = "--name",
-        description = "User name",
-        required = true
-    )
+  public static class RegularArgs {
+    @Parameter(names = {"--name", "-n"}, description = "User name", required = true)
     private String name;
+
+    @Parameter(names = "-v", required = true)
+    private int value;
+  }
+
+  public static class BrokenArgs {
+    @Parameter(names = {"--name", "-n"}, description = "User name", required = true)
+    private String name;
+
+    @Parameter(names = "-v", required = true, forceNonOverwritable = true)
+    private int value;
   }
 }
